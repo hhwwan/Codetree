5a57a4f25a0213584f5a2c4663f137c6d061b75d
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-09 16:10:03 +0900

    Title: 블럭쌓는 명령2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250609/블럭쌓는 명령2/block-stacking-commands2.py
+++ 250609/블럭쌓는 명령2/block-stacking-commands2.py
@@ -1,7 +1,7 @@
 n, k = map(int, input().split())
 commands = [tuple(map(int, input().split())) for _ in range(k)]
 
-answer = [0] * n
+answer = [0] * (n+1)
 
 for i in commands:
     for j in range(i[0], i[1]+1):
