54bfbafef76cd6778dc9bff7ab6c0c2e4bef72c8
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-10 15:03:30 +0900

    Title: 색종이의 총 넓이, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 250610/색종이의 총 넓이/the-total-area-of-colored-paper.py
+++ 250610/색종이의 총 넓이/the-total-area-of-colored-paper.py
@@ -5,7 +5,7 @@ x, y = list(x), list(y)
 
 answer = [[0] * 201 for _ in range(201)]
 
-for i in range(3):
+for i in range(n):
     for j in range(y[i],y[i]+8):
         for k in range(x[i],x[i]+8):
             if answer[j+100][k+100] == 0:
