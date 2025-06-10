05306aa21c87e19de994e672ea6345f7e99fc69c
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-10 15:37:22 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 146ms, Memory: 50MB, Status: Runtime Error - Codetree

--- 250610/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250610/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -17,15 +17,22 @@ for i in range(y1[1], y2[1]):
         if answer[i+1000][j+1000] == 1:
             answer[i+1000][j+1000] -= 1
 
-length, width = 0, 0
+min_x, max_x = 2001, -1
+min_y, max_y = 2001, -1
 
-# 세로 길이 구하기
-for a in answer:
-    if 1 in a:
-        length += 1
-# 가로 길이 구하기
-for b in answer:
-    if sum(b) > width:
-        width = sum(b)
+# 가로, 세로 길이 구하기
+for i in range(2001):
+    for j in range(2001):
+        if answer[i][j] == 1:
+            min_y = min(min_y, i)
+            max_y = max(max_y, i)
+            min_x = min(min_x, j)
+            max_x = max(max_x, j)
 
-print(length*width)
+if min_x == 2001:
+    print(0)
+else:
+    height = max_y - min_y + 1
+    width = max_x - min_x + 1
+
+print(height * width)
