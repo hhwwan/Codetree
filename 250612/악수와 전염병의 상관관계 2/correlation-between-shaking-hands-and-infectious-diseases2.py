b747e0e09a2388da607afb4536e6ef108d3e16a4
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-12 12:43:34 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250612/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250612/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 N, K, P, T = map(int, input().split())
 handshakes = [tuple(map(int, input().split())) for _ in range(T)]
 
@@ -11,29 +13,30 @@ answer = [0] * N
 answer[P-1] = 1
 virus[P] = K
 
-for time, a, b in sort_hand:
-    # 첫번째 개발자가 감염자 + 전염가능횟수 남음
-    if answer[a-1] == 1 and virus[a] > 0:
-        # 두번째 개발자가 감염당한적이 없는 경우
-        if answer[b-1] == 0:
-            answer[b-1] = 1
+# 시간별로 묶기
+time_map = defaultdict(list)
+for t, a, b in sort_hand:
+    time_map[t].append((a, b))
+
+# 시간순으로 처리
+for t in sorted(time_map.keys()):
+    today = time_map[t]
+
+    # 전염 시도 전에 현재 감염 상태 저장 (동시간 감염 방지)
+    infected_now = set()
+
+    for a, b in today:
+        if answer[a - 1] == 1 and virus[a] > 0:
+            infected_now.add((a, b))
+        if answer[b - 1] == 1 and virus[b] > 0:
+            infected_now.add((b, a))
+        
+    # 전염 처리
+    for a, b in infected_now:
+        if answer[b - 1] == 0:
+            answer[b - 1] = 1
             virus[b] = K
-        # 두번째 개발자도 이미 감염자인 경우 -> 아무것도 안해도 됨
-
-        # 전염 가능 횟수 -1
         virus[a] -= 1
-    # 첫번째 개발자가 감염자 + 전염가능횟수 없음 -> 아무것도 안해도 됨
-
-    # 두번째 개발자가 감염자 + 감염가능횟수 남음
-    if answer[b-1] == 1 and virus[b] > 0:
-        # 첫번째 개발자가 감염당한적인 없는 경우
-        if answer[a-1] == 0:
-            answer[a-1] = 1
-            virus[a] = K
-        # 첫번째 개발자가 이미 감염자인 경우 -> 아무것도 안해도 됨
-
-        # 전염 가능 횟수 -1
-        virus[b] -= 1
 
 for i in answer:
     print(i, end='')   
