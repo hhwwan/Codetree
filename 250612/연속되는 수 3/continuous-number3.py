eda7c5a8bea3489f572f6a129fa51526bfcc4dcf
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-12 10:04:34 +0900

    Title: 연속되는 수 3, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250612/연속되는 수 3/continuous-number3.py
+++ 250612/연속되는 수 3/continuous-number3.py
@@ -8,8 +8,12 @@ neg = 0 # 음수
 for i in range(N):
     if (i == 0 and arr[i] < 0) or (arr[i-1] > 0 and arr[i] < 0):
         neg += 1
+        if neg > answer:
+            answer = neg
     elif (i == 0 and arr[i] > 0) or (arr[i-1] < 0 and arr[i] > 0):
         pos += 1
+        if pos > answer:
+            answer = pos
     elif arr[i-1] < 0 and arr[i] < 0:
         pos = 0
         neg += 1
