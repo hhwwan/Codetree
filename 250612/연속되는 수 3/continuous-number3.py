f9152d19c36d19150a7791a3205f45a8d6fb22bc
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-12 10:09:43 +0900

    Title: 연속되는 수 3, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 250612/연속되는 수 3/continuous-number3.py
+++ 250612/연속되는 수 3/continuous-number3.py
@@ -1,28 +1,16 @@
 N = int(input())
 arr = [int(input()) for _ in range(N)]
 
-answer = 0
-pos = 0 # 양수
-neg = 0 # 음수
+answer = 1
+cnt = 1
 
-for i in range(N):
-    if (i == 0 and arr[i] < 0) or (arr[i-1] > 0 and arr[i] < 0):
-        neg += 1
-        if neg > answer:
-            answer = neg
-    elif (i == 0 and arr[i] > 0) or (arr[i-1] < 0 and arr[i] > 0):
-        pos += 1
-        if pos > answer:
-            answer = pos
-    elif arr[i-1] < 0 and arr[i] < 0:
-        pos = 0
-        neg += 1
-        if neg > answer:
-            answer = neg
-    elif arr[i-1] > 0 and arr[i] > 0:
-        neg = 0
-        pos += 1
-        if pos > answer:
-            answer = pos
+for i in range(1, N):
+    if (arr[i] > 0 and arr[i-1] > 0) or (arr[i] < 0 and arr[i-1] < 0):
+        cnt += 1
+        if cnt > answer:
+            answer = cnt
+    # 부호가 달라진다면 초기화
+    else:
+        cnt = 1
 
 print(answer)
