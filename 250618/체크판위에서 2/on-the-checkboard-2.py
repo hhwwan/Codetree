045cd9488b738892ce0bdb3870fbcb6fb5318096
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-18 10:33:20 +0900

    Title: 체크판위에서 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250618/체크판위에서 2/on-the-checkboard-2.py
+++ 250618/체크판위에서 2/on-the-checkboard-2.py
@@ -2,13 +2,19 @@ R, C = map(int, input().split())
 grid = [list(input().split()) for _ in range(R)]
 
 cnt = 0
+start = grid[0][0]
+end = grid[R-1][C-1]
 
 for i in range(1, R-1):
     for j in range(1, C-1):
-        if grid[i][j] != grid[0][0]:
-            for k in range(i+1, R-1):
-                for n in range(j+1, C-1):
-                    if grid[k][n] == grid[0][0]:
-                        cnt += 1
+        # 첫번째 점프 지점
+        if grid[i][j] == start:
+            continue
+        for k in range(i+1, R-1):
+            for l in range(j+1, C-1):
+                if grid[k][l] == grid[i][j]:
+                    continue
+                if grid[k][l] != end:
+                    cnt += 1
 
 print(cnt)
