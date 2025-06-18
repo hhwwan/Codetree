09478efb422055c930beb084b5a5c815e43035be
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-18 10:55:43 +0900

    Title: 이상한 진수 2, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250618/이상한 진수 2/awkward-digits-2.py
+++ 250618/이상한 진수 2/awkward-digits-2.py
@@ -2,9 +2,9 @@ a = input()
 
 answer = int(a,2)
 
-for i in range(1, len(a) + 1):
+for i in range(1, len(a)):
     if a[i] == '0':
-        answer += (2 ** (len(a) - i - 1))
+        answer += (2 ** (len(a) - 1 - i))
         break
 
 print(answer)
