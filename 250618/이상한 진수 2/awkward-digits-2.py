f2991ee62faa63584b40fd69168326632e8211e4
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-18 10:57:59 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250618/이상한 진수 2/awkward-digits-2.py
+++ 250618/이상한 진수 2/awkward-digits-2.py
@@ -1,10 +1,15 @@
 a = input()
 
 answer = int(a,2)
+change = False
 
 for i in range(1, len(a)):
     if a[i] == '0':
         answer += (2 ** (len(a) - 1 - i))
+        change = True
         break
+
+if not change:
+    answer -= 1
 
 print(answer)
