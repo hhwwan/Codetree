2926f618dad4e96c87752a5a5845b65b1937c044
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-19 10:01:05 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250619/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -7,20 +7,18 @@ y = [p[1] for p in points]
 
 min_distance = sys.maxsize
 
-for i in range(n-2):
-    num = 1
+for i in range(1, n-1):
     distance = 0
     start_x = x[0]
     start_y = y[0]
-    for j in range(1, n-1):
-        if j == num:
+    for j in range(1, n):
+        if j == i:
             continue
         distance += (abs(x[j]-start_x) + abs(y[j]-start_y))
         start_x = x[j]
         start_y = y[j]
-    distance += (abs(start_x - x[-1]) + abs(start_y - y[-1]))
+        
     if distance < min_distance:
         min_distance = distance
-    num += 1
 
 print(min_distance)
