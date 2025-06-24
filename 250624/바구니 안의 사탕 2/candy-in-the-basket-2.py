43f674510046bfd012256a044b6e4fbfc16f7aba
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-24 10:16:01 +0900

    Title: 바구니 안의 사탕 2, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250624/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250624/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -8,7 +8,7 @@ for _ in range(N):
     pos.append(p)
 
 # 위치에 사탕 채우기
-arr = [0] * (max(pos) + K + 1)
+arr = [0] * (100 + K + 1)
 
 for i in range(N):
     arr[pos[i]] += candy[i]
@@ -17,11 +17,8 @@ for i in range(N):
 max_candy = 0
 
 for c in range(len(arr)):
-    left = c - K
-    right = c + K
-
-    if left < 0 or right >= len(arr):
-        continue
+    left = max(0, c - K)
+    right = min(len(arr) - 1, c + K)
     
     tmp = 0
     for j in range(left, right + 1):
