156df1896dedeaac23cbb924729e8d76a8c54d50
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-25 15:46:52 +0900

    Title: 개발자의 능력 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250625/개발자의 능력 3/ability-of-developer-3.py
@@ -0,0 +1,15 @@
+abilities = list(map(int, input().split()))
+
+def team(a, b, c):
+    sum1 = a + b + c
+    sum2 = sum(abilities) - sum1
+    return abs(sum1 - sum2)
+
+min_value = 1000000
+
+for i in range(len(abilities)):
+    for j in range(i+1, len(abilities)):
+        for k in range(j+1, len(abilities)):
+            min_value = min(team(i, j, k), min_value)
+
+print(min_value)
