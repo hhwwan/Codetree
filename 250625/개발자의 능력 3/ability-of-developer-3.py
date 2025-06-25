508003c6d1eb74eaf92ffee03c2ae1e100c2a25a
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-25 15:49:12 +0900

    Title: 개발자의 능력 3, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250625/개발자의 능력 3/ability-of-developer-3.py
+++ 250625/개발자의 능력 3/ability-of-developer-3.py
@@ -10,6 +10,6 @@ min_value = 1000000
 for i in range(len(abilities)):
     for j in range(i+1, len(abilities)):
         for k in range(j+1, len(abilities)):
-            min_value = min(team(i, j, k), min_value)
+            min_value = min(team(abilities[i], abilities[j], abilities[k]), min_value)
 
 print(min_value)
