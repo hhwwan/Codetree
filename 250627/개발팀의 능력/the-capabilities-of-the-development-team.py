f456cf77bcce7021f7d444eb3a749195cedec008
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-06-27 18:40:27 +0900

    Title: 개발팀의 능력, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250627/개발팀의 능력/the-capabilities-of-the-development-team.py
+++ 250627/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -1,27 +1,28 @@
-from itertools import combinations
-
 arr = list(map(int, input().split()))
 
 min_value = float('inf')
-found = False
 
-# 5명 중 2명 먼저 team1로 선택
-for team1 in combinations(range(5), 2):
-    remain_people = [i for i in range(5) if i not in team1]
+for i in range(5):
+    team1, team2, team3 = 0, 0, 0
 
-    # 남은 3명 중 2명 고르기
-    for team2 in combinations(remain_people, 2):
-        team3 = [i for i in remain_people if i not in team2]
+    team1 = arr[i]
+    for j in range(5):
+        if j == i:
+            continue
+        else:
+            team2 = arr[j]
+        for k in range(5):
+            if k == i or k == j:
+                continue
+            else:
+                team2 += arr[k]
+            team3 = sum(arr) - (team1 + team2)
 
-        sum1 = arr[team1[0]] + arr[team1[1]]
-        sum2 = arr[team2[0]] + arr[team2[1]]
-        sum3 = arr[team3[0]]
+            if team1 != team2 and team1 != team3 and team2 != team3:
+                value = max(team1, team2, team3) - min(team1, team2, team3)
+                min_value = min(min_value, value)
 
-        if sum1 != sum2 and sum1 != sum3 and sum2 != sum3:
-            found = True
-            min_value = min(min_value, (max(sum1, sum2, sum3) - min(sum1, sum2, sum3)))
-
-if found:
+if min_value != 0:
     print(min_value)
 else:
     print(-1)
