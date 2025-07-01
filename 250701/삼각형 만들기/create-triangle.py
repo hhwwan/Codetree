d42a63dc8c03c2d170e46547f68f8a3ca0408952
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-07-01 12:16:57 +0900

    Title: 삼각형 만들기, Time: 121ms, Memory: 19MB, Status: Passed - Codetree

--- 250701/삼각형 만들기/create-triangle.py
+++ 250701/삼각형 만들기/create-triangle.py
@@ -3,28 +3,31 @@ points = [tuple(map(int, input().split())) for _ in range(n)]
 x = [p[0] for p in points]
 y = [p[1] for p in points]
 
-triangle_distance = 0
+max_area = 0
 
 for i in range(n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            x1, x2, x3 = x[i], x[j], x[k]
-            y1, y2, y3 = y[i], y[j], y[k]
+    for j in range(n):
+        for k in range(n):
+            if i == j or j == k or k == i:
+                continue
 
-            extent = 0
+            x1, y1 = x[i], y[i]
+            x2, y2 = x[j], y[j]
+            x3, y3 = x[k], y[k]
 
-            if y1 == y2 and x2 == x3:
-                extent = abs(x1 - x2) * abs(y2 - y3)
-            
-            elif y2 == y3 and x3 == x1:
-                extent = abs(x2 - x3) * abs(y3 - y1)
-            
-            elif y3 == y1 and x1 == x2:
-                extent = abs(x3 - x1) * abs(y1 - y2)
-        
-            triangle_distance = max(triangle_distance, extent)
+            area = 0
 
-if triangle_distance:
-    print(triangle_distance)
+            if y1 == y2 and x1 == x3 and y3 != y1 and x3 != x2:
+                area = abs(x1 - x2) * abs(y1 - y3)
+                max_area = max(max_area, area)
+            elif y2 == y3 and x2 == x1 and y1 != y2 and x1 != x3:
+                area = abs(x2 - x3) * abs(y2 - y1)
+                max_area = max(max_area, area)
+            elif y3 == y1 and x3 == x2 and y2 != y3 and x2 != x1:
+                area = abs(x3 - x1) * abs(y3 - y2)
+                max_area = max(max_area, area)
+
+if max_area:
+    print(max_area)
 else:
     print(0)
