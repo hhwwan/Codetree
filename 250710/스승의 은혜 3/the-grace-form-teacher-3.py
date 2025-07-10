cf8d4b23b9acff052f67fba85ce0672da9c4fa2e
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-07-10 14:20:49 +0900

    Title: 스승의 은혜 3, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250710/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -6,17 +6,27 @@ gifts.sort(key=lambda x : (x[0] + x[1], -x[0]))
 P = [gift[0] for gift in gifts]
 S = [gift[1] for gift in gifts]
 
-total = 0
-cnt = 0
+answer = 0
 for i in range(N):
-    total += P[i]
-    total += S[i]
-    if total <= B:
-        cnt += 1
-    else:
-        total -= (P[i] // 2)
+    cost_list = []
+    for j in range(N):
+        if i == j:
+            cost = P[j] // 2 + S[j]
+        else:
+            cost = P[j] + S[j]
+        cost_list.append(cost)
+
+    cost_list.sort()
+    total = 0
+    cnt = 0
+
+    for k in cost_list:
+        total += k
         if total <= B:
             cnt += 1
+        else:
             break
+    
+    answer = max(answer, cnt)
 
-print(cnt)
+print(answer)
