262a1eda6a6b8dda389a861b486c4906e8d475b3
김동환 <120583341+hhwwan@users.noreply.github.com>
2025-07-10 11:29:18 +0900

    Title: 이상한 폭탄 2, Time: 55ms, Memory: 16MB, Status: Passed - Codetree

--- 250710/이상한 폭탄 2/strange-bomb-2.py
+++ 250710/이상한 폭탄 2/strange-bomb-2.py
@@ -2,9 +2,10 @@ N, K = map(int, input().split())
 num = [int(input()) for _ in range(N)]
 
 boom = -1
-for i in range(N - K):
-    for j in range(i+1, N):
-        if num[i] == num[j]:
-            boom = max(boom, num[i])
+for i in range(N - 1):
+    for j in range(i + 1, i + 1 + K):
+        if j < N:
+            if num[i] == num[j]:
+                boom = max(boom, num[i])
 
 print(boom)
